18e9f7a8b72ed4ece7143c1810a18e4c5f5163c0
Elena Shashina <elvehnn@yandex.ru>
2022-07-29 16:31:51 +0300

    fix: fix eslint and prettier problems

--- Todo-list-Redux-and-tests/src/Components/Buttons/SelectAllItemsButton/SelectAllItemsButton.tsx
+++ Todo-list-Redux-and-tests/src/Components/Buttons/SelectAllItemsButton/SelectAllItemsButton.tsx
@@ -4,23 +4,23 @@ import { useAppSelector } from '../../../Store/hooks';
 import './SelectAllItemButton.scss';
 
 const SelectAllItemButton = () => {
-	const dispatch = useDispatch();
-	const isChecked = useAppSelector((state) => state.allButtonChecked);
-	const isListEmpty = useAppSelector((state) => state.todos).length === 0;
+  const dispatch = useDispatch();
+  const isChecked = useAppSelector((state) => state.allButtonChecked);
+  const isListEmpty = useAppSelector((state) => state.todos).length === 0;
 
-	return (
-		<input
-			className="input-buttons"
-			type="checkbox"
-			checked={isChecked}
-			disabled={isListEmpty}
-			onChange={() => {
-				dispatch(selectAllButtonChecked(!isChecked));
-				dispatch(toggleAllTodo(!isChecked));
-			}}
-			data-testid="select-all"
-		></input>
-	);
+  return (
+    <input
+      className="input-buttons"
+      type="checkbox"
+      checked={isChecked}
+      disabled={isListEmpty}
+      onChange={() => {
+        dispatch(selectAllButtonChecked(!isChecked));
+        dispatch(toggleAllTodo(!isChecked));
+      }}
+      data-testid="select-all"
+    ></input>
+  );
 };
 
 export default SelectAllItemButton;
--- Todo-list-Redux-and-tests/src/Store/reducer.ts
+++ Todo-list-Redux-and-tests/src/Store/reducer.ts
@@ -1,12 +1,12 @@
 import { Action, Todo } from '../Interfaces';
 import {
-	ADD_TODO,
-	DELETE_TODO,
-	FILTER_OPTIONS,
-	TOGGLE_TODO,
-	TOGGLE_ALL_TODO,
-	CLEAR_COMPLETED_TODOS,
-	EDIT_TODO,
+  ADD_TODO,
+  DELETE_TODO,
+  FILTER_OPTIONS,
+  TOGGLE_TODO,
+  TOGGLE_ALL_TODO,
+  CLEAR_COMPLETED_TODOS,
+  EDIT_TODO,
 } from './actions';
 import { combineReducers } from '@reduxjs/toolkit';
 import { routerReducer } from 'react-router-redux';
@@ -14,78 +14,80 @@ import { routerReducer } from 'react-router-redux';
 const storedTodos = localStorage.getItem('todos');
 
 export const todos = (
-	state: Todo[] = storedTodos ? JSON.parse(storedTodos) : [],
-	action: Action
+  state: Todo[] = storedTodos ? JSON.parse(storedTodos) : [],
+  action: Action
 ): Todo[] => {
-	switch (action.type) {
-		case ADD_TODO:
-			return [
-				...state,
-				{
-					id: state.length,
-					todo: action.text || '',
-					completed: false,
-				},
-			];
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          id: state.length,
+          todo: action.text || '',
+          completed: false,
+        },
+      ];
 
-		case CLEAR_COMPLETED_TODOS:
-			const activeTodos = state.filter((item) => !item.completed);
+    case CLEAR_COMPLETED_TODOS: {
+      const activeTodos = state.filter((item) => !item.completed);
 
-			return activeTodos.map((item, index) => {
-				return { ...item, id: index };
-			});
+      return activeTodos.map((item, index) => {
+        return { ...item, id: index };
+      });
+    }
 
-		case DELETE_TODO:
-			const newTodos = state.filter((item) => item.id !== action.index);
+    case DELETE_TODO: {
+      const newTodos = state.filter((item) => item.id !== action.index);
 
-			return newTodos.map((item, index) => {
-				return { ...item, id: index };
-			});
+      return newTodos.map((item, index) => {
+        return { ...item, id: index };
+      });
+    }
 
-		case TOGGLE_TODO:
-			return state.map((item: Todo, index) => {
-				if (index === action.index) {
-					return { ...item, completed: !item.completed };
-				}
-				return item;
-			});
+    case TOGGLE_TODO:
+      return state.map((item: Todo, index) => {
+        if (index === action.index) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      });
 
-		case EDIT_TODO:
-			return state.map((item: Todo, index) => {
-				if (index === action.newTodo?.id) {
-					return { ...item, todo: action.newTodo.todo };
-				}
-				return item;
-			});
+    case EDIT_TODO:
+      return state.map((item: Todo, index) => {
+        if (index === action.newTodo?.id) {
+          return { ...item, todo: action.newTodo.todo };
+        }
+        return item;
+      });
 
-		case TOGGLE_ALL_TODO:
-			return state.map((item: Todo) => {
-				return { ...item, completed: action.isChecked };
-			});
+    case TOGGLE_ALL_TODO:
+      return state.map((item: Todo) => {
+        return { ...item, completed: action.isChecked };
+      });
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
 
 export const filter = (state = FILTER_OPTIONS.SHOW_ALL, action: Action) => {
-	switch (action.type) {
-		case 'SET_FILTER':
-			return action.filter;
+  switch (action.type) {
+    case 'SET_FILTER':
+      return action.filter;
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
 
 export const allButtonChecked = (state = false, action: Action) => {
-	switch (action.type) {
-		case 'SELECT_ALL_ITEM_BUTTON':
-			return action.isChecked;
+  switch (action.type) {
+    case 'SELECT_ALL_ITEM_BUTTON':
+      return action.isChecked;
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
 
 const rootReducer = combineReducers({ routerReducer, todos, filter, allButtonChecked });
